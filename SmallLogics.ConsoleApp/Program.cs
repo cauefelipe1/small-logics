550650cd81c5b3b0e864dc9555bf2fab27faf2a6
Caue Carvalho <cauefelipe1@hotmail.com>
2025-02-10 15:25:08 -0300

    Adding code

---
+++ SmallLogics.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SmallLogics.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine(Test("abcded"));
+        Console.WriteLine(Test("abdd"));
+        Console.WriteLine(Test("aab"));
+        Console.WriteLine(Test("aaaaaabc"));
+    }
+    
+    private static string Test(string input)
+    {
+        var founds = new List<string>();
+        var previousChars = new HashSet<char>();
+        
+        previousChars.Add(input[0]);
+        int lastIdx = 0;
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            if (!previousChars.Add(input[i]))
+            {
+                founds.Add(input.Substring(lastIdx, i - lastIdx));
+                lastIdx = i;
+                previousChars.Clear();
+            }
+        }
+        
+        if (previousChars.Count > 0)
+            founds.Add(input.Substring(lastIdx, input.Length - lastIdx));
+        
+        
+        return founds.Count > 0 ? founds.First(f => f.Length == founds.Max(f => f.Length)) : input;
+    }
+}
